c7d7473b311ece432101fb4b07eb8759f466d1c2
Johnny <johnny@solanalysis.com>
2022-04-23 14:48:49 -0400

    add all fetch data client calls

--- src/HyperspaceClient.ts
+++ src/HyperspaceClient.ts
@@ -1,10 +1,40 @@
-import { getSdk, Sdk } from "./sdk";
-import { GraphQLClient } from 'graphql-request'
+import {
+  GetMarketPlaceActionsByProjectsCondition,
+  GetMarketPlaceActionsByTokenAddressCondition,
+  GetMarketPlaceActionsByUserCondition,
+  GetMarketPlaceSnapshotCondition,
+  GetMarketPlaceStateCondition,
+  GetMarketplaceStatusQuery,
+  GetProjectStatsCondition,
+  GetProjectStatsQuery,
+  getSdk,
+  GetTokenHistoryQuery,
+  GetTokenStateQuery,
+  GetUserBidsQuery,
+  GetUserHistoryQuery,
+  GetUserListingsQuery,
+  MarketPlaceActionEnum,
+  OrderConfig,
+  PaginationConfig,
+  Sdk,
+  SearchProjectByNameQuery,
+} from "./sdk";
+import { GraphQLClient } from "graphql-request";
+import {
+  GetMarketplaceSnapshotCondition,
+  GetMarketplaceStateCondition,
+  GetProjectHistoryCondition,
+  GetTokenHistoryCondition,
+  GetUserActionsCondition,
+  GetUserHistoryCondition,
+} from "./types";
 
-const apiEndpoint = process.env.NODE_ENV === 'production' ? "https://beta.api.solanalysis.com/sdk" : "http://localhost:8080";
+const apiEndpoint =
+  process.env.NODE_ENV === "production"
+    ? "https://beta.api.solanalysis.com/sdk"
+    : "http://localhost:8080";
 
 export class HyperspaceClient {
-
   apiKey: string;
   graphqlClient: GraphQLClient;
   sdk: Sdk;
@@ -16,40 +46,204 @@ export class HyperspaceClient {
   }
 
   // Getters
-  searchForProject(name: string) {
-    
+  searchProjectByName(name: string): Promise<SearchProjectByNameQuery> {
+    return this.sdk.searchProjectByName({
+      condition: {
+        display_name: name,
+      },
+    });
   }
 
-  getProjects(){
+  getProjects({
+    condition,
+    orderBy,
+    paginationInfo,
+  }: {
+    condition: GetProjectStatsCondition;
+    orderBy?: OrderConfig;
+    paginationInfo?: PaginationConfig;
+  }): Promise<GetProjectStatsQuery> {
+    return this.sdk.getProjectStats({
+      conditions: condition,
+      orderBy,
+      paginationInfo,
+    });
   }
 
-  getMarketplaceStatus() {
+  getMarketplaceStatus(): Promise<GetMarketplaceStatusQuery> {
+    return this.sdk.getMarketplaceStatus();
   }
 
-  getMarketplaceSnapshot() {
+  getMarketplaceSnapshot({
+    condition,
+    orderBy,
+    paginationInfo,
+  }: {
+    condition?: GetMarketplaceSnapshotCondition;
+    orderBy?: OrderConfig;
+    paginationInfo?: PaginationConfig;
+  }) {
+    const parsedCondition: GetMarketPlaceSnapshotCondition = {};
+    if (condition?.projects) parsedCondition.project_ids = condition.projects;
+    if (condition?.excludeTokensWithoutMetadata)
+      parsedCondition.has_metadata = true;
+    if (condition?.includeAttributeInformation)
+      parsedCondition.include_attribute_details = true;
+    if (condition?.includeProjectFloorPrice)
+      parsedCondition.include_floors = true;
+    if (condition?.onlyListings)
+      parsedCondition.listing_type = MarketPlaceActionEnum.Listing;
+    if (condition?.tokenAddresses)
+      parsedCondition.token_addresses = condition.tokenAddresses;
+    if (condition?.priceFilter)
+      parsedCondition.price_filter = condition.priceFilter;
+
+    return this.sdk.getMarketplaceSnapshots({
+      condition: parsedCondition,
+      order_by: orderBy,
+      pagination_info: paginationInfo,
+    });
   }
 
-  getTokenHistory() {
+  getTokenState({
+    condition,
+    orderBy,
+    paginationInfo,
+  }: {
+    condition: GetMarketplaceStateCondition;
+    orderBy?: OrderConfig;
+    paginationInfo?: PaginationConfig;
+  }): Promise<GetTokenStateQuery> {
+    const parsedCondition: GetMarketPlaceStateCondition = {
+      token_addresses: condition.tokenAddresses,
+    };
+    if (condition.actionType)
+      parsedCondition.action_type = condition.actionType;
+    if (condition.buyerAddress)
+      parsedCondition.buyer_address = condition.buyerAddress;
+    if (condition.sellerAddress)
+      parsedCondition.seller_address = condition.sellerAddress;
+    if (condition.marketPlaceIdentifiers)
+      parsedCondition.marketplace_ids = condition.marketPlaceIdentifiers;
+
+    return this.sdk.getTokenState({
+      //@ts-ignore
+      condition: parsedCondition,
+      orderBy,
+      paginationInfo,
+    });
   }
 
-  getUserHistory() {
+  getUserBids({
+    condition,
+    orderBy,
+  }: {
+    condition: GetUserActionsCondition;
+    orderBy?: OrderConfig;
+  }): Promise<GetUserBidsQuery>  {
+    const parsedCondition: GetMarketPlaceStateCondition = {
+      buyer_address: condition.userAddress,
+    };
+    if (condition.marketPlaceIdentifiers)
+      parsedCondition.marketplace_ids = condition.marketPlaceIdentifiers;
+    return this.sdk.getUserBids({
+      condition: parsedCondition,
+      order_by: orderBy,
+    });
+  }
+
+  getUserListings({
+    condition,
+    orderBy,
+  }: {
+    condition: GetUserActionsCondition;
+    orderBy?: OrderConfig;
+  }):  Promise<GetUserListingsQuery> {
+    const parsedCondition: GetMarketPlaceStateCondition = {
+      seller_address: condition.userAddress,
+    };
+    if (condition.marketPlaceIdentifiers)
+      parsedCondition.marketplace_ids = condition.marketPlaceIdentifiers;
+
+    return this.sdk.getUserListings({
+      condition: parsedCondition,
+      orderBy,
+    });
+  }
+
+  getTokenHistory({
+    condition,
+    orderBy,
+    paginationInfo,
+  }: {
+    condition: GetTokenHistoryCondition;
+    orderBy?: OrderConfig;
+    paginationInfo?: PaginationConfig;
+  }): Promise<GetTokenHistoryQuery>  {
+    const parsedCondition: GetMarketPlaceActionsByTokenAddressCondition = {
+      token_addresses: condition.tokenAddresses,
+    };
+    if (condition.actionType)
+      parsedCondition.action_type = condition.actionType;
+    if (condition.marketPlaceIdentifiers)
+      parsedCondition.marketplace_ids = condition.marketPlaceIdentifiers;
+
+    return this.sdk.getTokenHistory({
+      condition: parsedCondition,
+      orderBy,
+      paginationInfo,
+    });
+  }
+
+  getUserHistory({
+    condition,
+    paginationInfo,
+  }: {
+    condition: GetUserHistoryCondition;
+    paginationInfo?: PaginationConfig;
+  }):  Promise<GetUserHistoryQuery>  {
+    const parsedCondition: GetMarketPlaceActionsByUserCondition = {
+      seller_address: condition.userAddress,
+      buyer_address: condition.userAddress,
+    };
+
+    if (condition.actionTypes)
+      parsedCondition.by_mpa_types = condition.actionTypes;
+
+    return this.sdk.getUserHistory({
+      condition: parsedCondition,
+      paginationInfo,
+    });
+  }
+
+  getProjectHistory({
+    condition,
+    paginationInfo,
+  }: {
+    condition: GetProjectHistoryCondition;
+    paginationInfo?: PaginationConfig;
+  }) {
+    const parsedCondition: GetMarketPlaceActionsByProjectsCondition = {
+      projects: condition.projects,
+    };
+    if (condition.actionTypes)
+      parsedCondition.by_mpa_types = condition.actionTypes;
+    return this.sdk.getProjectHistory({
+      condition: parsedCondition,
+      paginationInfo,
+    });
   }
 
   // Marketplace Actions
-  createBuyTx() {
-  }
+  createBuyTx() {}
 
-  createListTx() {
-  }
+  createListTx() {}
 
-  createDelistTx() {
-  }
+  createDelistTx() {}
 
-  createBidTx() {
-  }
+  createBidTx() {}
 
-  createWithdrawEscrowTx() {
-  }
+  createWithdrawEscrowTx() {}
 
   getApiKey() {
     return this.apiKey;
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,53 @@
-import { PaginationConfig } from "./sdk";
+import {
+  MarketPlaceActionEnum,
+  MarketPlaceIdentifier,
+  MarketPlacePricingFilterValues,
+  ProjectIdWithAttributes,
+} from "./sdk";
+
+export type GetMarketplaceSnapshotCondition = {
+  projects?: ProjectIdWithAttributes[];
+  tokenAddresses?: string[];
+  excludeTokensWithoutMetadata?: boolean;
+  onlyListings?: boolean;
+  priceFilter?: MarketPlacePricingFilterValues;
+  includeAttributeInformation?: boolean;
+  includeProjectFloorPrice?: boolean;
+};
+
+export type MarketplaceActionEnums = MarketPlaceActionEnum.Bid
+| MarketPlaceActionEnum.Delisting
+| MarketPlaceActionEnum.Listing
+| MarketPlaceActionEnum.Transaction
+| MarketPlaceActionEnum.Updatelisting
+| MarketPlaceActionEnum.Updatebid
+| MarketPlaceActionEnum.Cancelbid;
+
+export type GetMarketplaceStateCondition = {
+  tokenAddresses: string[];
+  actionType?: MarketplaceActionEnums;
+  buyerAddress?: string;
+  sellerAddress?: string;
+  marketPlaceIdentifiers?: MarketPlaceIdentifier[];
+};
+// GetMarketPlaceActionsByTokenAddressCondition
+export type GetTokenHistoryCondition = {
+  tokenAddresses: string[];
+  actionType?: MarketplaceActionEnums;
+  marketPlaceIdentifiers?: MarketPlaceIdentifier[];
+}
+
+export type GetUserActionsCondition = {
+  userAddress: string;
+  marketPlaceIdentifiers?: MarketPlaceIdentifier[];
+}
+
+export type GetUserHistoryCondition = {
+  userAddress: string;
+  actionTypes?: MarketplaceActionEnums[];
+}
+
+export type GetProjectHistoryCondition = {
+  projects: ProjectIdWithAttributes[];
+  actionTypes?: MarketplaceActionEnums[];
+}
