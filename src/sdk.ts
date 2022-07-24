f08b3d9119abf87495d19a9df5bb4ecc945ff0ed
Johnny <johnny@solanalysis.com>
2022-07-23 21:40:04 -0400

    add project_description to snapshot response

--- src/sdk.ts
+++ src/sdk.ts
@@ -448,6 +448,7 @@ export type MarketPlaceSnapshotResponse = {
   non_market_place_state?: Maybe<NonMarketPlaceActions>;
   owner?: Maybe<Scalars['String']>;
   project_attributes?: Maybe<Array<ProjectAttribute>>;
+  project_description?: Maybe<Scalars['String']>;
   project_id: Scalars['String'];
   project_image?: Maybe<Scalars['String']>;
   project_name?: Maybe<Scalars['String']>;
@@ -1194,6 +1195,15 @@ export type ValidateAndConfirmSignatureOutput = {
   success: Scalars['Boolean'];
 };
 
+export type GetMarketplaceSnapshotsQueryVariables = Exact<{
+  condition?: InputMaybe<GetMarketPlaceSnapshotCondition>;
+  pagination_info?: InputMaybe<PaginationConfig>;
+  order_by?: InputMaybe<Array<OrderConfig> | OrderConfig>;
+}>;
+
+
+export type GetMarketplaceSnapshotsQuery = { __typename?: 'Query', getMarketPlaceSnapshots: { __typename?: 'GetMarketPlaceSnapshotsResponse', market_place_snapshots?: Array<{ __typename?: 'MarketPlaceSnapshotResponse', token_address: string, project_id: string, name?: string | null, rarity_est?: number | null, rank_est?: number | null, supply?: number | null, full_img?: string | null, meta_data_img?: string | null, meta_data_uri?: string | null, attributes?: any | null, floor_price?: number | null, project_name?: string | null, project_image?: string | null, project_slug?: string | null, project_description?: string | null, is_project_verified?: boolean | null, project_attributes?: Array<{ __typename?: 'ProjectAttribute', name: string, counts?: any | null, type: AttributeTypeEnum, values: Array<string> }> | null, lowest_listing_mpa?: { __typename?: 'MarketPlaceActionResponse', user_address?: string | null, price?: number | null, marketplace_program_id?: string | null, type?: MarketPlaceActionEnum | null, signature?: string | null, amount?: number | null, broker_referral_address?: string | null, block_timestamp?: number | null, broker_referral_fee?: number | null, escrow_address?: string | null, fee?: number | null, marketplace_fee_address?: string | null, marketplace_instance_id?: string | null, metadata?: any | null } | null, highest_bid_mpa?: { __typename?: 'MarketPlaceActionResponse', marketplace_fee_address?: string | null, fee?: number | null, escrow_address?: string | null, broker_referral_fee?: number | null, broker_referral_address?: string | null, block_timestamp?: number | null, signature?: string | null, amount?: number | null, type?: MarketPlaceActionEnum | null, marketplace_program_id?: string | null, marketplace_instance_id?: string | null, price?: number | null, user_address?: string | null, metadata?: any | null } | null }> | null, pagination_info: { __typename?: 'PaginationInfoResponseType', current_page_number: number, current_page_size: number, has_next_page: boolean, total_page_number: number } } };
+
 export type GetBuyTxQueryVariables = Exact<{
   buyerAddress: Scalars['String'];
   price: Scalars['Float'];
@@ -1315,15 +1325,6 @@ export type GetNonMpaUserHistoryQueryVariables = Exact<{
 
 export type GetNonMpaUserHistoryQuery = { __typename?: 'Query', getNonMpaUserHistory: { __typename?: 'GetMarketPlaceSnapshotsResponse', market_place_snapshots?: Array<{ __typename?: 'MarketPlaceSnapshotResponse', token_address: string, project_id: string, project_name?: string | null, name?: string | null, owner?: string | null, full_img?: string | null, rank_est?: number | null, meta_data_img?: string | null, is_project_verified?: boolean | null, non_market_place_state?: { __typename?: 'NonMarketPlaceActions', token_address?: string | null, signature?: string | null, source_address?: string | null, destination_address?: string | null, program_id?: string | null, collection_id?: string | null, new_authority?: string | null, price?: number | null, type?: NonMarketPlaceActionEnum | null, currency?: string | null, amount?: number | null, decimal?: number | null, destination_token_account?: string | null, source_token_account?: string | null, metadata?: any | null, block_timestamp?: number | null, block_number?: number | null } | null }> | null, pagination_info: { __typename?: 'PaginationInfoResponseType', current_page_number: number, current_page_size: number, has_next_page: boolean } } };
 
-export type GetMarketplaceSnapshotsQueryVariables = Exact<{
-  condition?: InputMaybe<GetMarketPlaceSnapshotCondition>;
-  pagination_info?: InputMaybe<PaginationConfig>;
-  order_by?: InputMaybe<Array<OrderConfig> | OrderConfig>;
-}>;
-
-
-export type GetMarketplaceSnapshotsQuery = { __typename?: 'Query', getMarketPlaceSnapshots: { __typename?: 'GetMarketPlaceSnapshotsResponse', market_place_snapshots?: Array<{ __typename?: 'MarketPlaceSnapshotResponse', token_address: string, project_id: string, name?: string | null, rarity_est?: number | null, rank_est?: number | null, supply?: number | null, full_img?: string | null, meta_data_img?: string | null, meta_data_uri?: string | null, attributes?: any | null, floor_price?: number | null, project_name?: string | null, project_image?: string | null, project_slug?: string | null, is_project_verified?: boolean | null, project_attributes?: Array<{ __typename?: 'ProjectAttribute', name: string, counts?: any | null, type: AttributeTypeEnum, values: Array<string> }> | null, lowest_listing_mpa?: { __typename?: 'MarketPlaceActionResponse', user_address?: string | null, price?: number | null, marketplace_program_id?: string | null, type?: MarketPlaceActionEnum | null, signature?: string | null, amount?: number | null, broker_referral_address?: string | null, block_timestamp?: number | null, broker_referral_fee?: number | null, escrow_address?: string | null, fee?: number | null, marketplace_fee_address?: string | null, marketplace_instance_id?: string | null, metadata?: any | null } | null, highest_bid_mpa?: { __typename?: 'MarketPlaceActionResponse', marketplace_fee_address?: string | null, fee?: number | null, escrow_address?: string | null, broker_referral_fee?: number | null, broker_referral_address?: string | null, block_timestamp?: number | null, signature?: string | null, amount?: number | null, type?: MarketPlaceActionEnum | null, marketplace_program_id?: string | null, marketplace_instance_id?: string | null, price?: number | null, user_address?: string | null, metadata?: any | null } | null }> | null, pagination_info: { __typename?: 'PaginationInfoResponseType', current_page_number: number, current_page_size: number, has_next_page: boolean, total_page_number: number } } };
-
 export type GetUserBidsQueryVariables = Exact<{
   condition?: InputMaybe<GetMarketPlaceStateCondition>;
   paginationInfo?: InputMaybe<PaginationConfig>;
@@ -1413,6 +1414,78 @@ export type GetWalletStatsHistQueryVariables = Exact<{
 export type GetWalletStatsHistQuery = { __typename?: 'Query', getWalletStatsHist: { __typename?: 'GetWalletStatsHistOutput', wallet_stats_history?: Array<{ __typename?: 'WalletStatHist', timestamp: number, portfolio_value?: number | null, owned_nfts?: number | null }> | null } };
 
 
+export const GetMarketplaceSnapshotsDocument = gql`
+    query getMarketplaceSnapshots($condition: GetMarketPlaceSnapshotCondition, $pagination_info: PaginationConfig, $order_by: [OrderConfig!]) {
+  getMarketPlaceSnapshots(
+    condition: $condition
+    pagination_info: $pagination_info
+    order_by: $order_by
+  ) {
+    market_place_snapshots {
+      token_address
+      project_id
+      name
+      rarity_est
+      rank_est
+      supply
+      full_img
+      meta_data_img
+      meta_data_uri
+      attributes
+      floor_price
+      project_name
+      project_image
+      project_slug
+      project_description
+      is_project_verified
+      project_attributes {
+        name
+        counts
+        type
+        values
+      }
+      lowest_listing_mpa {
+        user_address
+        price
+        marketplace_program_id
+        type
+        signature
+        amount
+        broker_referral_address
+        block_timestamp
+        broker_referral_fee
+        escrow_address
+        fee
+        marketplace_fee_address
+        marketplace_instance_id
+        metadata
+      }
+      highest_bid_mpa {
+        marketplace_fee_address
+        fee
+        escrow_address
+        broker_referral_fee
+        broker_referral_address
+        block_timestamp
+        signature
+        amount
+        type
+        marketplace_program_id
+        marketplace_instance_id
+        price
+        user_address
+        metadata
+      }
+    }
+    pagination_info {
+      current_page_number
+      current_page_size
+      has_next_page
+      total_page_number
+    }
+  }
+}
+    `;
 export const GetBuyTxDocument = gql`
     query getBuyTx($buyerAddress: String!, $price: Float!, $tokenAddress: String!, $buyerBroker: String!, $buyerBrokerBasisPoints: Float, $unverified: Boolean) {
   createBuyTx(
@@ -1737,77 +1810,6 @@ export const GetNonMpaUserHistoryDocument = gql`
       current_page_number
       current_page_size
       has_next_page
-    }
-  }
-}
-    `;
-export const GetMarketplaceSnapshotsDocument = gql`
-    query getMarketplaceSnapshots($condition: GetMarketPlaceSnapshotCondition, $pagination_info: PaginationConfig, $order_by: [OrderConfig!]) {
-  getMarketPlaceSnapshots(
-    condition: $condition
-    pagination_info: $pagination_info
-    order_by: $order_by
-  ) {
-    market_place_snapshots {
-      token_address
-      project_id
-      name
-      rarity_est
-      rank_est
-      supply
-      full_img
-      meta_data_img
-      meta_data_uri
-      attributes
-      floor_price
-      project_name
-      project_image
-      project_slug
-      is_project_verified
-      project_attributes {
-        name
-        counts
-        type
-        values
-      }
-      lowest_listing_mpa {
-        user_address
-        price
-        marketplace_program_id
-        type
-        signature
-        amount
-        broker_referral_address
-        block_timestamp
-        broker_referral_fee
-        escrow_address
-        fee
-        marketplace_fee_address
-        marketplace_instance_id
-        metadata
-      }
-      highest_bid_mpa {
-        marketplace_fee_address
-        fee
-        escrow_address
-        broker_referral_fee
-        broker_referral_address
-        block_timestamp
-        signature
-        amount
-        type
-        marketplace_program_id
-        marketplace_instance_id
-        price
-        user_address
-        metadata
-      }
-    }
-    pagination_info {
-      current_page_number
-      current_page_size
-      has_next_page
-      total_page_number
     }
   }
 }
@@ -2189,6 +2191,9 @@ const defaultWrapper: SdkFunctionWrapper = (action, _operationName, _operationTy
 
 export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
   return {
+    getMarketplaceSnapshots(variables?: GetMarketplaceSnapshotsQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetMarketplaceSnapshotsQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<GetMarketplaceSnapshotsQuery>(GetMarketplaceSnapshotsDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getMarketplaceSnapshots', 'query');
+    },
     getBuyTx(variables: GetBuyTxQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetBuyTxQuery> {
       return withWrapper((wrappedRequestHeaders) => client.request<GetBuyTxQuery>(GetBuyTxDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getBuyTx', 'query');
     },
@@ -2227,9 +2232,6 @@ export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = 
     },
     getNonMpaUserHistory(variables: GetNonMpaUserHistoryQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetNonMpaUserHistoryQuery> {
       return withWrapper((wrappedRequestHeaders) => client.request<GetNonMpaUserHistoryQuery>(GetNonMpaUserHistoryDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getNonMpaUserHistory', 'query');
-    },
-    getMarketplaceSnapshots(variables?: GetMarketplaceSnapshotsQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetMarketplaceSnapshotsQuery> {
-      return withWrapper((wrappedRequestHeaders) => client.request<GetMarketplaceSnapshotsQuery>(GetMarketplaceSnapshotsDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getMarketplaceSnapshots', 'query');
     },
     getUserBids(variables?: GetUserBidsQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetUserBidsQuery> {
       return withWrapper((wrappedRequestHeaders) => client.request<GetUserBidsQuery>(GetUserBidsDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getUserBids', 'query');
